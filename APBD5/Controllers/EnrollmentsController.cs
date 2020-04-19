12ccb7b455d5a27a34752052d2cade133fc6a73a
DoboszKacper <kacper_dobosz@onet.pl>
2020-04-19 15:07:38 +0200

    Initial Commit

---
+++ APBD5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,64 @@
+﻿using APBD5.Helpers;
+using APBD5.DTOs.RequestModels;
+using APBD5.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace APBD5.Controllers
+{
+    [ApiController]
+    [Route("api/enrollments")]
+    public class EnrollmentsController : ControllerBase
+    {
+
+        private readonly IStudentsDbService _dbService;
+
+        public EnrollmentsController(IStudentsDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(StudentWithStudiesRequest request)
+        {
+            try
+            {
+                return Ok(_dbService.CreateStudentWithStudies(request));
+            }
+            catch (DbServiceException e)
+            {
+                if (e.Type == DbServiceExceptionTypeEnum.NotFound)
+                    return NotFound(e.Message);
+                else if (e.Type == DbServiceExceptionTypeEnum.ValueNotUnique)
+                    return BadRequest(e.Message);
+                else
+                    return StatusCode(500);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost("promotions")]
+        public IActionResult PromoteStudents(PromotionRequest request)
+        {
+            if (!_dbService.CheckIfEnrollmentExists(request.Studies, request.Semester))
+                return NotFound("Enrollment not found.");
+
+            try
+            {
+                return Ok(_dbService.PromoteStudents(request.Studies, request.Semester));
+            }
+            catch (DbServiceException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.ToString());
+            }
+        }
+    }
+}
---
+++ APBD5/Services/IStudentsDbService.cs
@@ -0,0 +1,18 @@
+﻿using APBD5.Models;
+using APBD5.DTOs.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace APBD5.Services
+{
+    public interface IStudentsDbService
+    {
+        void CreateStudent(string indexNumber, string firstName, string lastName, DateTime birthDate, int idEnrollment, SqlConnection sqlConnection = null, SqlTransaction transaction = null);
+        Enrollment CreateStudentWithStudies(StudentWithStudiesRequest request);
+        Student GetStudent(string indexNumber);
+        IEnumerable<Student> GetStudents();
+        bool CheckIfEnrollmentExists(string studies, int semester);
+        Enrollment PromoteStudents(string studies, int semester);
+    }
+}
